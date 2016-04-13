ce6bbadb13f9ad7e19501c149db3cc68d2c0a57d
nikobo <nikola.boric@outlook.com>
2016-04-14 00:39:55 +0200

    no message

---
+++ AppliMVC/AppliMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AppliMVC.Startup))]
+namespace AppliMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
